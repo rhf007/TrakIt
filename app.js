d1f6f83a73f62887290699f67e8148ba296859ed
rhf007 <rahofahazem512@gmail.com>
2024-12-22 22:59:45 +0200

    set up EJS view engine and static file serving, add about route

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
+app.use(express.static('./public'))
+// Set custom views folder location
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.render('index')
 })
-
+// Route to handle other requests
+app.get('/about', (req, res) => {
+    /* res.sendFile(path.join(__dirname, 'public', 'about.html')); */
+    res.render('about')
+});
 app.listen(5000, () => {
     console.log('server listening on port 5000')
 })
