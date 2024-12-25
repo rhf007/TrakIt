c706a1953154a46e3d8f270bfe2ba2c80c2de56b
rhf007 <rahofahazem512@gmail.com>
2024-12-25 13:20:43 +0200

    add sign-in page, update routing and apply experimental style

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 // Route to handle other requests
-app.get('/about', (req, res) => {
+app.get('/sign-in', (req, res) => {
     /* res.sendFile(path.join(__dirname, 'public', 'about.html')); */
-    res.render('about')
+    res.render('sign-in')
 });
 app.listen(5000, () => {
     console.log('server listening on port 5000')
