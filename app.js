296d08c7a2a3d5dcefd7ea390f4222ab8d1973ce
rhf007 <rahofahazem512@gmail.com>
2024-12-26 10:09:25 +0200

    add register route

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const path = require('path')
 const app = express()
 
 app.use(express.static('./public'))
+
 // Set custom views folder location
 app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     res.render('index')
 })
-// Route to handle other requests
+
 app.get('/sign-in', (req, res) => {
-    /* res.sendFile(path.join(__dirname, 'public', 'about.html')); */
     res.render('sign-in')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register')
 });
 app.listen(5000, () => {
     console.log('server listening on port 5000')
