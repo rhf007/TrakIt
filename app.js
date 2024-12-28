c93cc520f9452171e53fe2c469305e4970a198a8
rhf007 <rahofahazem512@gmail.com>
2024-12-29 01:17:43 +0200

    refactor movie fetching

--- app.js
+++ app.js
@@ -15,55 +15,39 @@ app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs')
 
 app.get('/', async(req, res) => {
-    /* const Movies = []
-    const TV = []
-    let movie_slice = []
-    let tv_slice = []
-    const pages = 2
-    
-    try{
-    for (let i = 1; i <= pages; i++) {
-    const movie_results = await moviedb.movieNowPlaying({page: i})
-    Movies.push(...movie_results.results)
-    }
-    
-    movie_slice = Movies.slice(0, 30)
-    
-    const tv_results = await axios.get('https://api.tvmaze.com/shows')
-    
-    TV.push(...tv_results.data)
-    tv_slice = TV.slice(0, 30)
-    
-    res.render('index', {movies: movie_slice, tvs: tv_slice})
-    } catch(error){
-    console.error(`Error fetching movies: ${error}`);
-    res.status(500).send('Internal Server Error');
-    } */
-    //TODO: Limit reults to 15 cards
-    const now_playing_movies = []
-    const popular_movies = []
-    const top_rated_movies = []
-    const upcoming_movies =[]
+    let now_playing_movies = []
+    let popular_movies = []
+    let top_rated_movies = []
+    let upcoming_movies =[]
+
     try {
         const now_playing_results = await moviedb.movieNowPlaying()
         now_playing_movies.push(...now_playing_results.results)
+        const now_playing_slice = now_playing_movies.slice(0, 18)
+
         const popular_results = await moviedb.moviePopular()
         popular_movies.push(...popular_results.results)
+        const popular_slice = popular_movies.slice(0, 18)
+
         const top_rated_results = await moviedb.movieTopRated()
         top_rated_movies.push(...top_rated_results.results)
+        const top_rated_slice = top_rated_movies.slice(0, 18)
+
         const upcoming_results = await moviedb.upcomingMovies()
         upcoming_movies.push(...upcoming_results.results)
+        const upcoming_slice = upcoming_movies.slice(0, 18)
+
         res.render('index', {
-            now_playing_movies: now_playing_movies,
-            popular_movies: popular_movies,
-            top_rated_movies: top_rated_movies,
-            upcoming_movies: upcoming_movies})
-        } catch (error) {
+            now_playing_movies: now_playing_slice,
+            popular_movies: popular_slice,
+            top_rated_movies: top_rated_slice,
+            upcoming_movies: upcoming_slice
+        })} catch (error) {
             console.log(error)
             res.status(500).send('Internal Server Error')
         }
     })
-    
+
     app.get('/sign-in', (req, res) => {
         res.render('sign-in')
     });
