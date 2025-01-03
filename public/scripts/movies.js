f0bc72204e01753c9ce804f898e49f3f313ff1cc
rhf007 <rahofahazem512@gmail.com>
2025-01-03 11:28:27 +0200

    removed transparent navbar from movies since theres no need

--- public/scripts/movies.js
+++ public/scripts/movies.js
@@ -10,20 +10,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
 })
-//TODO: FIX THIS SINCE THERE'S NO MAIN SECTION IN THE MOVIES PAGE(LATER)
-/* document.addEventListener('scroll', () => {
-    const header = document.querySelector('.home-nav');
-    const firstSection = document.querySelector('.main');
-    const sectionHeight = firstSection.offsetHeight;
-    const scrollY = window.scrollY;
-
-    const progress = Math.min(scrollY / sectionHeight, 1);
-
-    const red = 220;
-    const green = 49;
-    const blue = 41;
-    const opacity = progress;
-
-    header.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
-    header.style.backdropFilter = `blur(${5 - opacity * 5}px)`;
-}); */
